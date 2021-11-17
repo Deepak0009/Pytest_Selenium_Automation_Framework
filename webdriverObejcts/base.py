884cb2f56c55e84f73c17e32af30d4c0d24677e6
ABC <deepakrsmishra@gmail.com>
2021-11-16 22:42:48 -0800

    newadd

--- webdriverObejcts/base.py
+++ webdriverObejcts/base.py
@@ -8,3 +8,18 @@ class Base:
     def verigyTitle(self):
         assert self.driver.title
 
+
+
+# git init : create .git directory in project directory
+
+# git clone https://github.com   : clone the repo from remote to local
+
+# git status : displays the state of the working directory and the staging area
+
+
+#git add : adds your modified files to the queue to be committed later.
+
+#git commit : commits the files that have been added and creates a new revision with a log
+
+
+#git push : pushes your changes to the remote repository.
